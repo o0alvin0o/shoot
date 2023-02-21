4a9a8f44e475d616ac73d38c0ac5758429ec72f2
Quoc Cuong Vo <o0cesc0o@gmail.com>
2023-02-21 21:33:41 +0700

    grep keywords in single thread

--- src/cli.rs
+++ src/cli.rs
@@ -1,38 +1,62 @@
-use std::path::PathBuf;
-use clap::Parser;
-
+use std::path::{Path, PathBuf};
+use clap::{Parser, Subcommand};
+use std::env;
+/// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 pub struct Cli {
+    /// The pattern to look for
+    #[arg(long, short)]
     pattern: String,
-    //mode: Mode,
-    path: PathBuf
+    /// The path to the file to read
+    path: Option<PathBuf>,
+    /// Sub-command
+    #[command(subcommand)]
+    command: Option<Commands>,
 }
 
 impl Cli {
-    fn new(pattern: &str, mode: &str) -> Self {
-        // let mode = match mode {
-        //     "strict" => Mode::Strict,
-        //     "soft" => Mode::Soft,
-        // };
-        let pattern = pattern.to_owned();
 
-        let path = match std::env::consts::OS {
-            "windows" => PathBuf::from(r"C:\"),
-            _ => PathBuf::from("/"),
-        };
-        Cli { pattern, path }
+    pub fn is_dir_search(&self) -> bool {
+        match &self.path {
+            Some(p) => {
+                p.is_dir()
+            },
+            None => false,
+        }
     }
 
-    pub fn pattern(&self) -> &str {
+    pub fn is_file_search(&self) -> bool {
+        match &self.path {
+            Some(p) => {
+                p.is_file()
+            },
+            None => false,
+        }
+    }
+
+    pub fn path(&self) -> PathBuf {
+        match &self.path {
+            Some(p) => p.clone(),
+            None => match env::consts::OS {
+                windows=> PathBuf::from("/"),
+                _ => PathBuf::from(r"C:\"),
+            },
+        }
+    }
+
+    pub fn pattern (&self) -> &str {
         &self.pattern
     }
 
-    pub fn path(&self) -> &PathBuf {
-        &self.path
+    pub fn validate_command(&mut self) {
+        // TODO
     }
 }
 
-enum Mode {
+#[derive(Subcommand)]
+enum Commands {
     Strict,
     Soft,
+    Directory,
+    File,
 }
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,60 @@
 
 mod cli;
 
+use std::fs::File;
+use std::io::{BufRead, BufReader, self, Write, Seek};
+use std::path::PathBuf;
+use std::sync::{Arc, Mutex};
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::thread;
 use cli::Cli;
 use clap::Parser;
+use colored::Colorize;
+use termcolor::{BufferWriter, Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+use walkdir::WalkDir;
+use colored::*;
 
 fn main() {
-    let args = Cli::parse();
-    let content = std::fs::read_to_string(args.path()).expect("could not read file");
-    for (i,line) in content.lines().enumerate() {
-        if line.contains(args.pattern()) {
-            println!("Found in line {i}:");
-            println!("{line}");
+    let command = Cli::parse();
+    let files = command.path();
+
+    if files.is_file() {
+        read_file(&files, command.pattern())
+    }
+    else if files.is_dir() {
+        for file in WalkDir::new(files).into_iter().filter_map(|e| e.ok()) {
+            if file.metadata().unwrap().is_file() {
+                let path_buf = PathBuf::from(&file.path());
+                read_file(&path_buf, command.pattern())
+            }
         }
     }
 
 }
+
+
+fn read_file(path_buf: &PathBuf, pattern: &str) {
+    let file = File::open(path_buf);
+
+    let reader = match file {
+        Ok(f) => BufReader::new(f),
+        Err(_) => return,
+    };
+    
+    for (i,line) in reader.lines().enumerate() {
+        let line = match line {
+            Ok(l) => l,
+            Err(_) => return,
+        };
+
+        if line.contains(pattern) {
+            let number_of_line = (i + 1).to_string().cyan();
+            let file_path = path_buf.display().to_string().magenta();
+            println!("Line {} of file {}", number_of_line, file_path);
+            let line_to_print = line.replace(pattern, &pattern.green().to_string());
+            println!("{}", line_to_print);
+        }
+    }
+}
+
+
