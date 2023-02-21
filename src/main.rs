d5d29261f10a4b80a4f7a3c07c29515f35683eeb
Quoc Cuong Vo <o0cesc0o@gmail.com>
2023-02-21 23:04:47 +0700

    grep multi-thread

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,8 @@
 mod cli;
 
 use std::fs::File;
-use std::io::{BufRead, BufReader, self, Write, Seek};
+use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
-use std::sync::{Arc, Mutex};
-use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread;
 use cli::Cli;
 use clap::Parser;
@@ -14,24 +12,49 @@ use colored::Colorize;
 use termcolor::{BufferWriter, Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 use walkdir::WalkDir;
 use colored::*;
+use std::sync::mpsc;
+use std::thread::JoinHandle;
+use std::time::Duration;
 
+const MAX_THREADS: usize = 4;
 fn main() {
+    let max_thread_warning = "Max thead".red();
     let command = Cli::parse();
     let files = command.path();
+    let mut handles: Vec<JoinHandle<()>> = Vec::new();
 
     if files.is_file() {
         read_file(&files, command.pattern())
     }
     else if files.is_dir() {
         for file in WalkDir::new(files).into_iter().filter_map(|e| e.ok()) {
-            if file.metadata().unwrap().is_file() {
-                let path_buf = PathBuf::from(&file.path());
-                read_file(&path_buf, command.pattern())
+            println!("Current handles number : {}", handles.len());
+            if handles.len() == MAX_THREADS {
+                println!("{max_thread_warning}");
+                for _ in 0..MAX_THREADS {
+                    handles.pop().unwrap().join().unwrap();
+                }
             }
+
+            let cloned_keyword = String::from(command.pattern());
+            let path_buf = PathBuf::from(&file.path());
+
+            if file.metadata().unwrap().is_file() {
+                let handle = thread::spawn(move || {
+                    read_file(&path_buf, &cloned_keyword);
+                });
+                handles.push(handle);
+            }
+
+
+        }
+        for handle in handles {
+            handle.join().unwrap();
         }
     }
-
 }
+
+
 
 
 fn read_file(path_buf: &PathBuf, pattern: &str) {
